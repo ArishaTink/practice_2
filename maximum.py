39900add79f91a1a4a655ae9a71e3229e26d4426
Arisha <selestinatink@mail.ru>
2024-12-21 11:15:07 +0300

    maximum.py:modified

--- maximum.py
+++ maximum.py
@@ -1,7 +1,8 @@
+count = int(input("Введите кол-во строк в массиве "))
 string = input("Введите строку (Для выхода нажмите enter) ")
 string_array = []
 
-while string != "" :
+for i in range(1,count):
     string = input("Введите строку (Для выхода нажмите enter) ")
     string_array.append(string)
 
